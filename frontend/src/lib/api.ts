01ae2d89db1073b1740dd851bb6cc34107df83bc
rijul04 <rijul04@DESKTOP-1JLBHQP.>
2025-03-31 22:03:38 +0100

    shadcn downloaded

--- frontend/src/lib/api.ts
+++
@@ -1,5 +0,0 @@
-export async function fetchTestModels() {
-  const res = await fetch("http://localhost:8000/api/test_model/");
-  if (!res.ok) throw new Error("Failed to fetch subjects");
-  return res.json();
-}
