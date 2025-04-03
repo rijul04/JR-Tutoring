825ba751a80ff8f6e4522fa52445cef501e8ee87
ri1jgo0l <ri1jgo0l@gmail.com>
2025-04-03 04:50:19 +0100

    resizing better

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,3 +1,7 @@
+import { mergeClasses } from "@fluentui/react-components";
+
+import styles from "./Hero.module.css";
+
 type Props = {
   background: string;
   children: React.ReactNode;
@@ -10,32 +14,17 @@ export default function Hero({
   className,
 }: Readonly<Props>) {
   return (
-    <>
-      {/* putting logic to center stuff here for now but probably best to just have css done to the child
-       before passing see how easy or hard it is to do that? Otherwise extract into a css module file*/}
-      <section
-        style={{
-          backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          minHeight: "50vh",
-          alignItems: "center",
-          justifyContent: "center",
-          display: "flex",
-          //   flexDirection: "column",
-        }}
+    <section
+      className={styles.hero}
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      <div
+        className={mergeClasses(className ? className : "", styles.children)}
       >
-        <div
-          className={className}
-          style={{
-            maxWidth: "min(1200px, 100vw)",
-            margin: "0 auto",
-            padding: "0 1.5rem",
-          }}
-        >
-          {children}
-        </div>
-      </section>
-    </>
+        {children}
+      </div>
+    </section>
   );
 }
